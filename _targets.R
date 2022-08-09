49f9df46ebab7ba73f727bd29d9fb298a92b5259
kaizadp <50244730+kaizadp@users.noreply.github.com>
2022-08-09 14:42:46 -0700

    update targets. import metadata files. process and run weoc.

--- _targets.R
+++ _targets.R
@@ -17,18 +17,28 @@ tar_option_set(
 
 # Load the R scripts with your custom functions:
 source("2-code/0-packages.R")
-source("2-code/optode_processing.R")
-
+source("2-code/0b-initial_processing.R")
+source("2-code/1-functions_optode_processing.R")
+source("2-code/2-functions_chemistry_processing.R")
+source("2-code/3-functions_analysis.R")
 
 # list of targets
 list(
+  tar_target(sample_key, read.csv("1-data/sample_key.csv")),
+  tar_target(sample_weights, read.csv("1-data/sample_weights.csv")),
+  tar_target(analysis_key, read.csv("1-data/analysis_key.csv")),
+  
   # optode
   tar_target(optode_data, import_optode_data("1-data/optodes")),
   tar_target(optode_map, read_sheet("1Pumt5ZA2Ojc8Ow-Ex-gH63ChtTtZs1gPKc50UM098rY") %>% 
                mutate(optode_disc_number = as.character(optode_disc_number))),
-  tar_target(sample_key, read_sheet("1ZngRDe_jdiXKymQBaR5MI-F5sGS4rakoAkf6yYJ0YgQ")),
   tar_target(optode_data_processed, process_optode_data(optode_data, optode_map, sample_key)),
   tar_target(gg_optode, plot_optode_data(optode_data_processed)),
+  
+  # weoc
+  tar_target(weoc_data, import_weoc_data(FILEPATH = "1-data/npoc", PATTERN = "Summary")),
+  tar_target(weoc_processed, process_weoc(weoc_data, analysis_key)),
+  tar_target(gg_weoc, plot_weoc(weoc_processed)),
   
   # report
   tar_render(report, path = "3-reports/anoxia_report.Rmd")
