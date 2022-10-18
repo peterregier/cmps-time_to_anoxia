970e8a3a1103b6714cdc47a666453631dafd5068
Kaizad Patel <50244730+kaizadp@users.noreply.github.com>
2022-10-17 21:14:58 -0700

    add palette for horizons. plot new combined optode data. anoxia time summary. update plots for weoc, ions, ph

--- 2-code/2-functions_analysis.R
+++ 2-code/2-functions_analysis.R
@@ -1,4 +1,8 @@
 
+
+pal_horizons = soilpalettes::soil_palette("eutrostox", 2)
+
+
 # OPTODES ----
 plot_optode_data_all_samples = function(optode_data_processed){
   
@@ -34,18 +38,18 @@ plot_optode_data_all_samples = function(optode_data_processed){
        individual = individual)
 }
 
-plot_optode_data = function(optode_data_processed){
+plot_optode_data_OLD = function(optode_data_processed){
   
   grouped = 
     optode_data_processed %>% 
     filter(!grepl("skip", notes)) %>% 
     filter(!is.na(location)) %>% 
     filter(location != "water") %>% 
-    filter(timepoint == "24-hour") %>% 
-    ggplot(aes(x = time_minutes, y = corrected_do_mg_L, color = sample_name))+
+    filter(timepoint == c("24-hour", "2-week-rep")) %>% 
+    ggplot(aes(x = time_minutes/60, y = corrected_do_mg_L, color = sample_name))+
     geom_line()+
     labs(#title = "Time to Anoxia",
-      x = "Elapsed time, minutes",
+      x = "Elapsed time, hours",
       y = "Dissolved oxygen, mg/L")+
     #geom_smooth(se = F)+
     facet_grid(timepoint ~ location)+
@@ -82,6 +86,63 @@ plot_optode_data = function(optode_data_processed){
   
   list(grouped = grouped,
        individual = individual)
+}
+
+
+plot_optode_data = function(optode_data_processed){
+  
+  optode_combined2 = 
+    optode_data_processed %>% 
+    mutate(sample_type = location) %>% 
+    separate(sample_type, sep = "-", into = c("transect", "horizon")) %>% 
+    mutate(transect = factor(transect, levels = c("upland", "transition", "wetland")))
+  
+  gg_24hr = 
+    optode_combined2 %>% 
+    filter(!grepl("skip", notes)) %>% 
+    filter(!is.na(location)) %>% 
+    filter(location != "water") %>% 
+    filter(timepoint == c("24-hour")) %>% 
+    filter(timepoint == c("24-hour", "2-week-rep")) %>% 
+    ggplot(aes(x = time_minutes/60, y = corrected_do_mg_L, color = horizon, group = sample_name))+
+    geom_line()+
+    scale_color_manual(values = pal_horizons)+
+    labs(#title = "Time to Anoxia",
+      x = "Elapsed time, hours",
+      y = "Dissolved oxygen, mg/L")+
+    #geom_smooth(se = F)+
+    facet_grid(. ~ transect)+
+    theme(legend.position = "none")    
+  
+  
+  gg_2wk = 
+    optode_combined2 %>% 
+    filter(!grepl("skip", notes)) %>% 
+    filter(!is.na(location)) %>% 
+    filter(location != "water") %>% 
+    filter(timepoint == c("2-week-rep")) %>% 
+    ggplot(aes(x = (time_minutes/60)/24, y = corrected_do_mg_L, color = location, group = location))+
+    geom_line(size = 1)+
+    #   scale_color_manual(values = soilpalettes::soil_palette("podzol", 5))+
+    #      scale_color_manual(values = PNWColors::pnw_palette("Sunset2", 4))+
+    
+    scale_color_manual(values = c("#cc5c76", "#f9ad2a", "#625a94", "#1d457f"))+
+    
+    labs(#title = "Time to Anoxia",
+      x = "Elapsed time, days",
+      y = "Dissolved oxygen, mg/L")+
+    #geom_smooth(se = F)+
+    # facet_grid(timepoint ~ .)+
+    theme(legend.position = c(0.5,0.5))  +
+    NULL
+  
+  cowplot::plot_grid(gg_24hr + ggtitle("A: 24-hour"), gg_2wk + ggtitle("B: 2-week\n\n"), 
+                     rel_widths = c(2.7,1),
+                     label_y = 1,
+                     label_x = 0)
+  
+  
+  
 }
 
 compute_optode_slope_OLD = function(optode_data_processed){
@@ -189,6 +250,20 @@ compute_optode_slope = function(optode_data_processed){
     dplyr::select(-ends_with("START"), -ends_with("STOP"), ends_with("START"), ends_with("STOP")) %>% 
     mutate(do_slope = (do_mg_L_START - do_mg_L_STOP) / ((time_minutes_STOP - time_minutes_START)/60))
     
+  anoxia_time_summary = 
+    anoxia_time %>% 
+    group_by(location, timepoint) %>%
+    dplyr::summarise(mean_slope = mean(do_slope))
+    
+  
+  anoxia_time_24hr = 
+    anoxia_time %>% 
+    filter(timepoint == "24-hour")
+  
+  a = aov(do_slope ~ location, data = anoxia_time_24hr)
+  summary(a)
+  
+  h = agricolae::HSD.test(a, "location")
   
   
 }
@@ -204,21 +279,26 @@ plot_weoc = function(weoc_processed, sample_key){
   
   mg_l = 
     weoc %>% 
-    ggplot(aes(x = timepoint, y = npoc_corr_mgL))+
-    geom_point()+
-    facet_wrap(~location)+
+    ggplot(aes(x = timepoint, y = npoc_corr_mgL, shape = horizon, color = horizon))+
+    geom_point(size = 2, stroke = 1)+
+    scale_shape_manual(values = c(19, 1))+
+    scale_color_manual(values = pal_horizons)+
+    facet_wrap(~transect)+
     labs(title = "Water-extractable organic carbon",
          x = "", 
          y = "WEOC, mg/L")
   
   ug_g = 
     weoc %>% 
-    ggplot(aes(x = timepoint, y = npoc_ug_g))+
-    geom_point()+
-    facet_wrap(~location)+
+    ggplot(aes(x = timepoint, y = npoc_ug_g, shape = horizon, color = horizon))+
+    geom_point(size = 2, stroke = 1)+
+    scale_shape_manual(values = c(19, 1))+
+    scale_color_manual(values = pal_horizons)+
+    facet_wrap(~transect)+
     labs(title = "Water-extractable organic carbon",
          x = "", 
-         y = "WEOC, μg/g soil")
+         y = "WEOC, μg/g soil")+
+    theme(legend.position = "top")
   
   list(mg_l = mg_l,
        ug_g = ug_g)
@@ -231,26 +311,30 @@ plot_weoc = function(weoc_processed, sample_key){
 plot_ions = function(ions_processed, sample_key){
   ions = 
     ions_processed %>% 
-    left_join(sample_key)
+    left_join(sample_key) %>% 
+    recode_levels() 
 
   ions_mg_l = 
     ions %>% 
-    recode_levels() %>% 
     filter(location != "blank-filter") %>% 
-    ggplot(aes(x = timepoint, y = amount_ppm))+
-    geom_point()+
-    facet_wrap(ion ~ location, scales = "free_y")+
+    ggplot(aes(x = timepoint, y = amount_ppm, color = horizon, shape = horizon))+
+    geom_point(size = 2, stroke = 1)+
+    scale_shape_manual(values = c(19, 1))+
+    scale_color_manual(values = pal_horizons)+
+    facet_grid(ion ~ transect, scales = "free_y")+
     labs(title = "Dissolved ions",
          x = "", 
          y = "Ion concentrations, mg/L")  
   
   ions_ug_g =
     ions %>% 
-    recode_levels() %>% 
     filter(location != "blank-filter") %>% 
-    ggplot(aes(x = timepoint, y = amount_ug_g))+
-    geom_point()+
-    facet_wrap(ion ~ location, scales = "free_y")+
+    ggplot(aes(x = timepoint, y = amount_ug_g, color = horizon, shape = horizon))+
+    geom_point(size = 2, stroke = 1)+
+    scale_shape_manual(values = c(19, 1))+
+    scale_color_manual(values = pal_horizons)+
+    expand_limits(y = 0)+
+    facet_grid(ion ~ transect, scales = "free_y")+
     labs(title = "Dissolved ions",
          x = "", 
          y = "Ion concentrations, μg/g soil")
@@ -270,9 +354,11 @@ plot_pH = function(pH_processed, sample_key){
     pH %>% 
     recode_levels() %>% 
     filter(location != "blank-filter") %>% 
-    ggplot(aes(x = timepoint, y = pH))+
-    geom_point()+
-    facet_wrap(. ~ location)+
+    ggplot(aes(x = timepoint, y = pH, color = horizon, shape = horizon))+
+      geom_point(size = 2, stroke = 1)+
+      scale_shape_manual(values = c(19, 1))+
+      scale_color_manual(values = pal_horizons)+
+      facet_wrap(. ~ transect)+
     labs(title = "pH",
          x = "", 
          y = "pH")  
